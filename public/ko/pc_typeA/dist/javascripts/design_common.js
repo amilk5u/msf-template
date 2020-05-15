9ad6c50a3766ac150566a3871acec645760c83d5
amilk5u <amilk5u@gmail.com>
2020-05-15 14:24:54 +0900

    msf - template upload

--- public/ko/pc_typeA/dist/javascripts/design_common.js
+++ public/ko/pc_typeA/dist/javascripts/design_common.js
@@ -67,7 +67,7 @@ function main() {
     var $slide02 = $slideSection.find(".slide02");
     var $slide03 = $slideSection.find(".slide03");
 
-    var $slide02_cont = $slide02.find("slide_container");
+    var $slide02_cont = $slide02.find(".slide_container");
     var $slide02Li = $slide02_cont.find("li");
 
     var swiper = new Swiper('.swiper-container.slide03', {
@@ -80,8 +80,6 @@ function main() {
     });
 
 
-
-
     /*
     *  1. li 중 1번(0번)을 클릭시 index 0을 뱉어 낸다.
     *  2. li 중
@@ -89,17 +87,17 @@ function main() {
     *
     */
 
-
-    function test(index) {
+    function test() {
 
         var _this = $(this);
         var _index = _this.index();
 
         $slide02Li.on("click",function(){
-            console.log(_index);
         });
+
     }
-    test(1);
+    test();
+
 
 
 }
--- public/ko/pc_typeA/src/javascripts/function/main.js
+++ public/ko/pc_typeA/src/javascripts/function/main.js
@@ -7,7 +7,7 @@ function main() {
     var $slide02 = $slideSection.find(".slide02");
     var $slide03 = $slideSection.find(".slide03");
 
-    var $slide02_cont = $slide02.find("slide_container");
+    var $slide02_cont = $slide02.find(".slide_container");
     var $slide02Li = $slide02_cont.find("li");
 
     var swiper = new Swiper('.swiper-container.slide03', {
@@ -20,8 +20,6 @@ function main() {
     });
 
 
-
-
     /*
     *  1. li 중 1번(0번)을 클릭시 index 0을 뱉어 낸다.
     *  2. li 중
@@ -29,17 +27,17 @@ function main() {
     *
     */
 
-
-    function test(index) {
+    function test() {
 
         var _this = $(this);
         var _index = _this.index();
 
         $slide02Li.on("click",function(){
-            console.log(_index);
         });
+
     }
-    test(1);
+    test();
+
 
 
 }
